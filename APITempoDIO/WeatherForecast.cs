d0954fe3e0019c63ab6b986abc6725ab302040dc
Maieru <67087888+Maieru@users.noreply.github.com>
2025-01-29 10:59:22 -0300

    Update WeatherForecast.cs
    Burro

--- APITempoDIO/WeatherForecast.cs
+++ APITempoDIO/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace APITempoDIO
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public int TemperaturK => 273.16 + TemperatureC;
+        public double TemperaturK => 273.16 + TemperatureC;
 
         public string? Summary { get; set; }
     }
