df3958d4a6d90493da9079ed4b8539d644e8d1d1
Maieru <67087888+Maieru@users.noreply.github.com>
2025-01-29 10:56:34 -0300

    Update WeatherForecast.cs

--- APITempoDIO/WeatherForecast.cs
+++ APITempoDIO/WeatherForecast.cs
@@ -8,6 +8,8 @@ namespace APITempoDIO
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        public int TemperaturK => 273.16 + TemperatureC;
+
         public string? Summary { get; set; }
     }
 }
